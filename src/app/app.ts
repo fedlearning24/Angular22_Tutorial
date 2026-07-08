00f8ec7fcdd5e52024ea2799667b42137cda6cd5
sushma <sushhonna08@gmail.com>
2026-07-08 20:49:20 +0530

    data binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { User } from './user/user';
+import { DataBinding } from './data-binding/data-binding';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [DataBinding],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/data-binding/data-binding.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  imports: [],
+  templateUrl: './data-binding.html',
+  styleUrl: './data-binding.css',
+})
+export class DataBinding {
+  name : string = "FED Learning";
+
+  topic : string = "4 Types of Data Binding..!";
+
+  image : string = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGm3-EPGReO14nVGJKy2EWN5Fd1hWhZEQ3TrXjNaXw5A&s=10";
+}
+
