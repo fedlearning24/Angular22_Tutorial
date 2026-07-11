d42566fe2dee830516a089f0deb4451d1bcaf782
sushma <sushhonna08@gmail.com>
2026-07-11 13:33:42 +0530

    event binding

--- src/app/data-binding/data-binding.ts
+++ src/app/data-binding/data-binding.ts
@@ -7,10 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './data-binding.css',
 })
 export class DataBinding {
-  name : string = "FED Learning";
 
-  topic : string = "4 Types of Data Binding..!";
+  name: string = "FED Learning";
 
-  image : string = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGm3-EPGReO14nVGJKy2EWN5Fd1hWhZEQ3TrXjNaXw5A&s=10";
+  topic: string = "4 Types of Data Binding..!";
+
+  image: string = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGm3-EPGReO14nVGJKy2EWN5Fd1hWhZEQ3TrXjNaXw5A&s=10";
+
+  onSubmitClick(){
+    alert("Data Saved Successfully.");
+  }
+
+  onCountryChange(){
+    alert("Country has changed.");
+  }
 }
 
